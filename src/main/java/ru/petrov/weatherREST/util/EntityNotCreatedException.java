dc2444b220afec75d15fa83115fbe35ac092f443
Aleksei Petrov <peta0982@gmail.com>
2023-05-13 16:31:08 +0500

    refactor naming, changed depricated annotation

--- src/main/java/ru/petrov/weatherREST/util/EntityNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package ru.petrov.weatherREST.util;
-
-public class EntityNotCreatedException extends RuntimeException {
-    public EntityNotCreatedException(String message) {
-        super(message);
-    }
-}
